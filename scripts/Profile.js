3d0702bf97f8cad1e77af88cc1a5d2f1cba8088f
Subhradeep <subhradeepbasu2002@gmail.com>
2024-08-22 01:50:40 +0530

    Update profile section styling

--- scripts/Profile.js
+++ scripts/Profile.js
@@ -1,4 +1,8 @@
-import { account } from "../appwrite/app.js";
+import { account, databases } from "../appwrite/app.js";
+import { config } from "../appwrite/config.js"
+import { Query } from "appwrite";
+
+
 
 document.addEventListener('DOMContentLoaded', () => {
     const profileName = document.getElementById('profile-name');
@@ -14,12 +18,47 @@ document.addEventListener('DOMContentLoaded', () => {
                 profileName.textContent = response.name;
                 profileEmail.textContent = response.email;
                 // Assuming response has role data
-                profileRole.textContent = response.role || 'User';
+                
             })
             .catch(error => {
                 console.error('Failed to fetch account details:', error);
             });
     }
 
+    async function getAccount(){
+        try{
+            const user = await account.get();
+            const userId = user.$id;
+    
+            console.log('User ID:', userId);
+    
+            const response = await databases.listDocuments(
+                config.DATABASE_ID,
+                config.ACCOUNTS_COLLECTION_ID,
+                [
+                    Query.equal('userId', userId)
+                ]
+            )
+    
+            if(response.total > 0){
+                const document = response.documents[0];
+    
+                console.log('User role:', document.role);
+                profileRole.textContent = document.role;
+                return{
+                    userId: document.userId,
+                    role: document.role
+                };
+            }else{
+                console.error('User not found');
+                return null;
+            }
+        }catch(error){
+            console.error('Failed to fetch user role:', error);
+            return null;
+        }
+    }
+
     displayLogin();
+    getAccount();
 });
